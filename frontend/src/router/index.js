ca2d00fd63e40588fa7b3744b1fbe6eff66893bb
Dewa Saputra <dewabandis11@gmail.com>
2023-09-18 10:41:37 +0700

    frontend default

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Landing from '@/views/Landing/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'landing',
+      component: () => import('@/views/Landing/index.vue')
     },
     {
       path: '/about',
@@ -15,8 +15,16 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      component: () => import('@/views/About/index.vue')
+    },
+	{
+		path: '/todo',
+		name: 'todo',
+		// route level code-splitting
+		// this generates a separate chunk (About.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () => import('@/views/TodoList/index.vue')
+	  }
   ]
 })
 
---
+++ frontend/src/views/TodoList/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<H1> Dewa Banget</H1>
+	</div>
+</template>
+
+<script>
+export default {
+	setup () {
+		
+
+		return {}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
